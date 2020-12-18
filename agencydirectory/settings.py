9c22f888bd577cd912aa616ef3b3c4151e022989
leigh90 <zawadikibathi@gmail.com>
2020-12-18 09:22:19 +0300

    Configure wsgi.py

--- agencydirectory/settings.py
+++ agencydirectory/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sw1ib^eb_f%+y3&m)=6f+_kjim#xgaan7n+xz!b@4u)=n!b8t8'
+SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
